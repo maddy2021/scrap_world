1c50e87629ecc12f4b8b47d9c5b48ff73829dc85
maddy2021 <45945046+maddy2021@users.noreply.github.com>
2021-08-08 14:47:32 +0530

    added initial code for multi Threading.

--- shopee_scraper.py
+++ shopee_scraper.py
@@ -3,6 +3,9 @@ import json
 import urllib
 from item_obj import Item_Obj
 import os
+from concurrent.futures import ThreadPoolExecutor
+
+MAX_THREADS = 10
 
 # SEARCH_LINK = "https://shopee.sg/api/v4/search/search_items?by=relevancy&keyword=tv&limit=60&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2"
 # https://shopee.sg/api/v2/item/get?itemid={item_id}&shopid={shop_id}
@@ -19,12 +22,13 @@ class ShopeeScraper():
         self.scraped_items = []
         self.get_item_links()
        
-
     def get_item_links(self):
         for data in self.search_data:
             new_count = 0
             limit = 60
             total_count = 1
+            session = requests.Session()
+            # try:
             while new_count <= total_count:
                 print("total count"+str(total_count))
                 print("new count"+str(new_count))
@@ -35,40 +39,35 @@ class ShopeeScraper():
                 total_count = search_json_data["total_count"]
                 # first_item = items_data[0]
                 count = 0
-                for item in items_data:
-                    if(count<5):
-                        print("processing count:"+str(count))
-                        shop_id = item["item_basic"]["shopid"]
-                        item_id = item["item_basic"]["itemid"]
-                        item_link = f"https://shopee.sg/api/v2/item/get?itemid={item_id}&shopid={shop_id}"
-                        item_object = Item_Obj()
-                        item_object.set_link(item_link)
-                        self.scrap_item_data(item_link,item,item_object)
-                        self.scraped_items.append(item_object.__dict__)
-                        count = count + 1
+                item_url_list = [f'https://shopee.sg/api/v2/item/get?itemid={item["item_basic"]["itemid"]}&shopid={item["item_basic"]["shopid"]}' for item in items_data ]
+                print(f'Under process url count {len(item_url_list)}')
+                threads = min(MAX_THREADS, len(item_url_list))
+                with ThreadPoolExecutor() as executor:
+                    executor.map(self.scrap_item_data,item_url_list,items_data)
                 new_count = new_count + 60
             item_data_json = self.scraped_items
             file_path = os.path.join("resource",self.domain_name,data)
             with open(file_path+'/item_data.json', 'w', encoding='utf-8') as fp:
                 json.dump(item_data_json, fp, ensure_ascii=False, indent=4)
             self.scraped_items = []
-
-
-    def scrap_item_data(self,item_link,item,item_object: Item_Obj):
+        
+    def scrap_item_data(self,item_link,item):
+        # session = requests.Session()
         item_data = requests.get(item_link,headers=headers).json()
+        item_object = Item_Obj()
+        item_object.set_link(item_link)
         self.scrape_name(item_data,item_object)
         self.scrape_desc(item_data,item_object)
         self.scrape_brand(item_data,item_object)
         self.scrape_discount(item_data,item_object)
         self.scrape_org_price(item,item_object)
         self.scrape_new_price(item,item_object)
+        self.scraped_items.append(item_object.__dict__)
 
     def scrape_name(self,item_data,item_object: Item_Obj):
         item_name = item_data["item"]["name"]
         item_object.set_name(item_name)
     
-    # def scrape_link(self,item_data,item_object):
-    #     self.link = link
     def scrape_org_price(self,item_data,item_object:Item_Obj):
         try:
             item_minprice = str(item_data["item_basic"]["price_min_before_discount"]/100000)
